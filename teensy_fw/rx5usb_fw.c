3a8521a051d1631687ec4729715b815970bb5657
GliGli <sfx.gligli@free.fr>
2016-10-18 00:05:27 +0200

    firmware USB detection fix -- programming speedup

--- teensy_fw/rx5usb_fw.c
+++ teensy_fw/rx5usb_fw.c
@@ -26,150 +26,117 @@ volatile uint8_t do_output=0;
 uint8_t send_buffer[64];
 uint8_t recv_buffer[64];
 
-char detect_RX5_power(void)
-{
-	char detected;
-	unsigned char od,op;
-
-	// set A0..A7 as pulled up inputs, so:
-	// - when the cartridge isn't plugged, we read 0xff
-	// - when the cartridge is plugged, we read 0x00
-	
-	od=DDRD;
-	op=PORTD;
-	
-	DDRD=0x00;
-	PORTD=0xff;
-	
-	_delay_ms(100); // the RX5 might need some time to output proper zero address
-	
-	detected=PIND!=0xff; 
-	
-	DDRD=od;
-	PORTD=op;
-	
-	return detected;
-}
-
 int main(void)
 {
 	unsigned long flash_addr,flash_size,flash_pos;
 	int8_t r,i;
 
-	CPU_PRESCALE(CPU_8MHz);
-
 	flash_setEnable(0);
 	
-	if(detect_RX5_power())
+	// Initialize the USB, and then wait for the host to set configuration.
+	// If the Teensy is powered without a PC connected to the USB port,
+	// this will wait forever.
+	usb_init();
+	while (!usb_configured()) /* wait */ ;
+
+	CPU_PRESCALE(CPU_16MHz);
+
+	for(;;)
 	{
-		CPU_PRESCALE(CPU_62kHz);
-		for(;;);	
-	}
-	else
-	{
-		// Initialize the USB, and then wait for the host to set configuration.
-		// If the Teensy is powered without a PC connected to the USB port,
-		// this will wait forever.
-		usb_init();
-		while (!usb_configured()) /* wait */ ;
+		// Wait an extra second for the PC's operating system to load drivers
+		// and do whatever it does to actually be ready for input
+		_delay_ms(1000);
+
+		print("RX5USB chip program mode\n");
+
+		print("waiting header...\n");
 
 		for(;;)
 		{
-			// Wait an extra second for the PC's operating system to load drivers
-			// and do whatever it does to actually be ready for input
-			_delay_ms(1000);
+			r = usb_rawhid_recv(recv_buffer,RX5USB_HID_TIMEOUT);
 
-			print("RX5USB chip program mode\n");
-
-			print("waiting header...\n");
-
-			for(;;)
+			if (r > 0 && recv_buffer[0]=='R'&& recv_buffer[1]=='X'&& recv_buffer[2]=='5' && recv_buffer[3]==RX5USB_FW_VERSION)
 			{
-				r = usb_rawhid_recv(recv_buffer,RX5USB_HID_TIMEOUT);
-
-				if (r > 0 && recv_buffer[0]=='R'&& recv_buffer[1]=='X'&& recv_buffer[2]=='5' && recv_buffer[3]==RX5USB_FW_VERSION)
-				{
-					flash_addr=*(unsigned long*)&recv_buffer[4];
-					flash_size=*(unsigned long*)&recv_buffer[8];
-					break;
-				}
+				flash_addr=*(unsigned long*)&recv_buffer[4];
+				flash_size=*(unsigned long*)&recv_buffer[8];
+				break;
 			}
+		}
 
-			print("a "); phex16(((uint32_t)flash_addr)>>16); phex16(flash_addr);
-			print("\ns "); phex16(((uint32_t)flash_size)>>16); phex16(flash_size);
-			print("\n");
+		print("a "); phex16(((uint32_t)flash_addr)>>16); phex16(flash_addr);
+		print("\ns "); phex16(((uint32_t)flash_size)>>16); phex16(flash_size);
+		print("\n");
 
-			print("flash start...\n");
+		print("flash start...\n");
 
-			flash_setEnable(1);
+		flash_setEnable(1);
 
-			print("id: ");
-			if(!flash_checkId())
-			{
-				print("bad id!\n");
-				for(;;);
-			}
+		print("id: ");
+		if(!flash_checkId())
+		{
+			print("bad id!\n");
+			for(;;);
+		}
 
-			print("erase...\n");
+		print("erase...\n");
 #if 1
-			flash_eraseBlocks(flash_addr,flash_size);
+		flash_eraseBlocks(flash_addr,flash_size);
 #else
-			flash_eraseChip();
+		flash_eraseChip();
 #endif			
 
-			print("ack header...\n");
+		print("ack header...\n");
 
-			// resend the header, to ack it
+		// resend the header, to ack it
 
-			memcpy(send_buffer,recv_buffer,sizeof(recv_buffer));
+		memcpy(send_buffer,recv_buffer,sizeof(recv_buffer));
 
-			send_buffer[12]='O';
-			send_buffer[13]='K';
+		send_buffer[12]='O';
+		send_buffer[13]='K';
+
+		do
+			r = usb_rawhid_send(send_buffer,RX5USB_HID_TIMEOUT);
+		while (r<=0);
+
+		print("chip program...\n");
+
+		flash_pos=0;
+
+		while(flash_pos<flash_size)
+		{
+			// get packet
+
+			do
+				r = usb_rawhid_recv(recv_buffer,RX5USB_HID_TIMEOUT);
+			while (r!=sizeof(recv_buffer));
+
+			// program it
+
+			for(i=0;i<sizeof(recv_buffer);++i)
+			{
+				flash_programByte(flash_addr+flash_pos,recv_buffer[i]);
+				++flash_pos;
+			}
+
+			// send what we red back from flash
+
+			for(i=0;i<sizeof(send_buffer);++i)
+			{
+				send_buffer[i]=flash_getByte(flash_addr+flash_pos-sizeof(send_buffer)+i);
+			}
 
 			do
 				r = usb_rawhid_send(send_buffer,RX5USB_HID_TIMEOUT);
-			while (r<=0);
+			while (r!=sizeof(send_buffer));
 
-			print("chip program...\n");
-
-			flash_pos=0;
-
-			while(flash_pos<flash_size)
+			if((flash_pos&0x3fff)==0)
 			{
-				// get packet
-
-				do
-					r = usb_rawhid_recv(recv_buffer,RX5USB_HID_TIMEOUT);
-				while (r!=sizeof(recv_buffer));
-
-				// program it
-
-				for(i=0;i<sizeof(recv_buffer);++i)
-				{
-					flash_programByte(flash_addr+flash_pos,recv_buffer[i]);
-					++flash_pos;
-				}
-
-				// send what we red back from flash
-
-				for(i=0;i<sizeof(send_buffer);++i)
-				{
-					send_buffer[i]=flash_getByte(flash_addr+flash_pos-sizeof(send_buffer)+i);
-				}
-
-				do
-					r = usb_rawhid_send(send_buffer,RX5USB_HID_TIMEOUT);
-				while (r!=sizeof(send_buffer));
-
-				if((flash_pos&0x7ff)==0)
-				{
-					print(".");
-				}
+				print(".");
 			}
-
-			print("all done!\n");
-
-			flash_setEnable(0);
 		}
+
+		print("all done!\n");
+
+		flash_setEnable(0);
 	}
 }
--- teensy_fw/usb_rawhid_debug.c
+++ teensy_fw/usb_rawhid_debug.c
@@ -49,9 +49,9 @@
 // all, but allocating more than necessary means reserved
 // bandwidth is no longer available to other USB devices.
 #define RAWHID_TX_SIZE		64	// transmit packet size
-#define RAWHID_TX_INTERVAL	2	// max # of ms between transmit packets
+#define RAWHID_TX_INTERVAL	1	// max # of ms between transmit packets
 #define RAWHID_RX_SIZE		64	// receive packet size
-#define RAWHID_RX_INTERVAL	8	// max # of ms between receive packets
+#define RAWHID_RX_INTERVAL	1	// max # of ms between receive packets
 
 
 /**************************************************************************
@@ -99,7 +99,7 @@ static const uint8_t PROGMEM endpoint_config_table[] = {
 // spec and relevant portions of any USB class specifications!
 
 
-static uint8_t PROGMEM device_descriptor[] = {
+static const uint8_t PROGMEM device_descriptor[] = {
 	18,					// bLength
 	1,					// bDescriptorType
 	0x00, 0x02,				// bcdUSB
@@ -116,7 +116,7 @@ static uint8_t PROGMEM device_descriptor[] = {
 	1					// bNumConfigurations
 };
 
-static uint8_t PROGMEM rawhid_hid_report_desc[] = {
+static const uint8_t PROGMEM rawhid_hid_report_desc[] = {
 	0x06, LSB(RAWHID_USAGE_PAGE), MSB(RAWHID_USAGE_PAGE),
 	0x0A, LSB(RAWHID_USAGE), MSB(RAWHID_USAGE),
 	0xA1, 0x01,				// Collection 0x01
@@ -132,7 +132,7 @@ static uint8_t PROGMEM rawhid_hid_report_desc[] = {
 	0xC0					// end collection
 };
 
-static uint8_t PROGMEM debug_hid_report_desc[] = {
+static const uint8_t PROGMEM debug_hid_report_desc[] = {
 	0x06, 0x31, 0xFF,			// Usage Page 0xFF31 (vendor defined)
 	0x09, 0x74,				// Usage 0x74
 	0xA1, 0x53,				// Collection 0x53
@@ -148,7 +148,7 @@ static uint8_t PROGMEM debug_hid_report_desc[] = {
 #define CONFIG1_DESC_SIZE        (9+9+9+7+7+9+9+7)
 #define RAWHID_HID_DESC_OFFSET   (9+9)
 #define DEBUG_HID_DESC_OFFSET    (9+9+9+7+7+9)
-static uint8_t PROGMEM config1_descriptor[CONFIG1_DESC_SIZE] = {
+static const uint8_t PROGMEM config1_descriptor[CONFIG1_DESC_SIZE] = {
 	// configuration descriptor, USB spec 9.6.3, page 264-266, Table 9-10
 	9, 					// bLength;
 	2,					// bDescriptorType;
@@ -229,17 +229,17 @@ struct usb_string_descriptor_struct {
 	uint8_t bDescriptorType;
 	int16_t wString[];
 };
-static struct usb_string_descriptor_struct PROGMEM string0 = {
+static const struct usb_string_descriptor_struct PROGMEM string0 = {
 	4,
 	3,
 	{0x0409}
 };
-static struct usb_string_descriptor_struct PROGMEM string1 = {
+static const struct usb_string_descriptor_struct PROGMEM string1 = {
 	sizeof(STR_MANUFACTURER),
 	3,
 	STR_MANUFACTURER
 };
-static struct usb_string_descriptor_struct PROGMEM string2 = {
+static const struct usb_string_descriptor_struct PROGMEM string2 = {
 	sizeof(STR_PRODUCT),
 	3,
 	STR_PRODUCT
@@ -247,7 +247,7 @@ static struct usb_string_descriptor_struct PROGMEM string2 = {
 
 // This table defines which descriptor data is sent for each specific
 // request from the host (in wValue and wIndex).
-static struct descriptor_list_struct {
+static const struct descriptor_list_struct {
 	uint16_t	wValue;
 	uint16_t	wIndex;
 	const uint8_t	*addr;
